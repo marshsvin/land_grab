05176c5cfcbf268e5ac47b02b13db5fecd3140fd
Raghu Betina <raghu@firstdraft.com>
2017-05-11 20:11:42 +0000

    Whitelist columns in PropertyInfo for ActiveAdmin

--- app/admin/property_info.rb
+++ app/admin/property_info.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register PropertyInfo do
 
+ permit_params :landowner_id, :property_address, :property_zip_code, :property_city, :property_state, :property_acreage, :property_description, :property_pictures
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
