8a58c08c86126f6333e223ce6b62300dc6adef83
Raghu Betina <raghu@firstdraft.com>
2017-05-11 20:11:16 +0000

    Direct association between user_types and property_reviews

--- app/models/property_review.rb
+++ app/models/property_review.rb
@@ -1,6 +1,10 @@
 class PropertyReview < ApplicationRecord
   # Direct associations
 
+  belongs_to :user,
+             :class_name => "UserType",
+             :counter_cache => true
+
   # Indirect associations
 
   # Validations
